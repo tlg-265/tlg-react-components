23c26b9bca40f31dbd439be70ab9f8bec350b606
Angel Hernandez <angel@theleadgroup.com>
2018-12-17 22:37:39 -0600

    example simplified

--- tlg-react-components-tester/src/components/App.js
+++ tlg-react-components-tester/src/components/App.js
@@ -5,17 +5,10 @@ import React, { Component } from "react";
 
 import ReactColorSquare from "react-color-square";
 
-import DayChecker from "./DayChecker";
-import Header from "./Header";
-import MealPlan from "./MealPlan";
-
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Header />
-        <DayChecker />
-        <MealPlan />
         <ReactColorSquare width={150} color="red" text="Hello World!" />
       </div>
     );
