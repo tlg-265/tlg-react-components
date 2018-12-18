9070887d90d11505c2dc476d923894a407d4be39
Angel Hernandez <angel@theleadgroup.com>
2018-12-17 23:49:03 -0600

    adding component: MarketplacePartnerModal, but pending to fix: componentWillReceiveProps

--- tlg-react-components-tester/src/components/App.js
+++ tlg-react-components-tester/src/components/App.js
@@ -1,8 +1,126 @@
 import React, { Component } from "react";
-
-import { ReactColorSquare, FancyCheckboxTextInput } from "tlg-react-components";
+import ReactDOM from 'react-dom';
+import { ReactColorSquare, FancyCheckboxTextInput, MarketplacePartnerModal } from "tlg-react-components";
+import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = Object.assign({}, {
+      /* default argument values */
+      marketplacePartners: [
+        [ 'Partner 01', 'License 01' ],
+        [ 'Partner 02', 'License 02' ],
+        [ 'Partner 03', 'License 03' ],
+        [ 'Partner 04', 'License 04' ],
+        [ 'Partner 05', 'License 05' ],
+        [ 'Partner 06', 'License 06' ],
+        [ 'Partner 01', 'License 01' ],
+        [ 'Partner 02', 'License 02' ],
+        [ 'Partner 03', 'License 03' ],
+        [ 'Partner 04', 'License 04' ],
+        [ 'Partner 05', 'License 05' ],
+        [ 'Partner 06', 'License 06' ],
+        [ 'Partner 01', 'License 01' ],
+        [ 'Partner 02', 'License 02' ],
+        [ 'Partner 03', 'License 03' ],
+        [ 'Partner 04', 'License 04' ],
+        [ 'Partner 05', 'License 05' ],
+        [ 'Partner 06', 'License 06' ],
+        [ 'Partner 01', 'License 01' ],
+        [ 'Partner 02', 'License 02' ],
+        [ 'Partner 03', 'License 03' ],
+        [ 'Partner 04', 'License 04' ],
+        [ 'Partner 05', 'License 05' ],
+        [ 'Partner 06', 'License 06' ],
+        [ 'Partner 01', 'License 01' ],
+        [ 'Partner 02', 'License 02' ],
+        [ 'Partner 03', 'License 03' ],
+        [ 'Partner 04', 'License 04' ],
+        [ 'Partner 05', 'License 05' ],
+        [ 'Partner 06', 'License 06' ],
+        [ 'Partner 01', 'License 01' ],
+        [ 'Partner 02', 'License 02' ],
+        [ 'Partner 03', 'License 03' ],
+        [ 'Partner 04', 'License 04' ],
+        [ 'Partner 05', 'License 05' ],
+        [ 'Partner 06', 'License 06' ],
+        [ 'Partner 01', 'License 01' ],
+        [ 'Partner 02', 'License 02' ],
+        [ 'Partner 03', 'License 03' ],
+        [ 'Partner 04', 'License 04' ],
+        [ 'Partner 05', 'License 05' ],
+        [ 'Partner 06', 'License 06' ],
+      ],
+    }, props);
+
+  }
+
+  componentDidMount() {
+    const thisRoot = ReactDOM.findDOMNode(this);
+    const whatever_child = thisRoot.querySelector('.whatever_child');
+    console.log(whatever_child);
+  }
+
+  makeAjaxCall = (event) => {
+    event.preventDefault();
+    this.refs.modal_marketplace_partners.show();
+    
+    // whatever Ajax call here and after finshed, do the following:
+    // simulating delay with: "setTimeout"...
+
+    setTimeout(() => {
+      this.setState({
+        marketplacePartners: [
+          [ 'Partner 01', 'License 01' ],
+          [ 'Partner 02', 'License 02' ],
+          [ 'Partner 03', 'License 03' ],
+          [ 'Partner 04', 'License 04' ],
+          [ 'Partner 05', 'License 05' ],
+          [ 'Partner 06', 'License 06' ],
+          [ 'Partner 01', 'License 01' ],
+          [ 'Partner 02', 'License 02' ],
+          [ 'Partner 03', 'License 03' ],
+          [ 'Partner 04', 'License 04' ],
+          [ 'Partner 05', 'License 05' ],
+          [ 'Partner 06', 'License 06' ],
+          [ 'Partner 01', 'License 01' ],
+          [ 'Partner 02', 'License 02' ],
+          [ 'Partner 03', 'License 03' ],
+          [ 'Partner 04', 'License 04' ],
+          [ 'Partner 05', 'License 05' ],
+          [ 'Partner 06', 'License 06' ],
+          [ 'Partner 01', 'License 01' ],
+          [ 'Partner 02', 'License 02' ],
+          [ 'Partner 03', 'License 03' ],
+          [ 'Partner 04', 'License 04' ],
+          [ 'Partner 05', 'License 05' ],
+          [ 'Partner 06', 'License 06' ],
+          [ 'Partner 01', 'License 01' ],
+          [ 'Partner 02', 'License 02' ],
+          [ 'Partner 03', 'License 03' ],
+          [ 'Partner 04', 'License 04' ],
+          [ 'Partner 05', 'License 05' ],
+          [ 'Partner 06', 'License 06' ],
+          [ 'Partner 01', 'License 01' ],
+          [ 'Partner 02', 'License 02' ],
+          [ 'Partner 03', 'License 03' ],
+          [ 'Partner 04', 'License 04' ],
+          [ 'Partner 05', 'License 05' ],
+          [ 'Partner 06', 'License 06' ],
+          [ 'Partner 01', 'License 01' ],
+          [ 'Partner 02', 'License 02' ],
+          [ 'Partner 03', 'License 03' ],
+          [ 'Partner 04', 'License 04' ],
+          [ 'Partner 05', 'License 05' ],
+          [ 'Partner 06', 'License 06' ],
+        ]
+      });
+    }, 1000);
+  }
+
   render() {
     return (
       <div className="container">
@@ -18,6 +136,17 @@ class App extends Component {
         <div style={{ width: '500px' }}>
           <FancyCheckboxTextInput name="debt_03" text="I Have Debt 03" title="Debt 03" />
         </div>
+
+        <MarketplacePartnerModal
+          open={false}
+          ref="modal_marketplace_partners"
+          logo="https://d36wlm0wgeem6j.cloudfront.net/img/logo.png?v=##BUILD_NUMBER##"
+          marketplacePartners={this.state.marketplacePartners}
+          buttonCloseColor="#71c12c"
+          rowColor="#dddbcf"
+        />
+
+        <a href="/" onClick={ this.makeAjaxCall }>Marketplace Partners</a>
 
       </div>
     );
--- tlg-react-components/src/index.js
+++ tlg-react-components/src/index.js
@@ -1,7 +1,9 @@
 import ReactColorSquare from './ReactColorSquare/ReactColorSquare';
 import FancyCheckboxTextInput from './FancyCheckboxTextInput/FancyCheckboxTextInput';
+import MarketplacePartnerModal from './MarketplacePartnerModal/MarketplacePartnerModal';
 
 export {
   ReactColorSquare,
   FancyCheckboxTextInput,
+  MarketplacePartnerModal,
 };
